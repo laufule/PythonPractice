480020901ec58e7829f5e1b775c9be620fc2529c
laufule <laufule@gmail.com>
2020-05-14 23:17:04 +0800

    add something

--- 4.2-list.loop.py
+++ 4.2-list.loop.py
@@ -3,8 +3,13 @@ bickles = ['one','two','three','four','five']
 for bickle in bickles:
     print(bickle)
 
+# 循环中赋值
 car = []
 for value in range(1,10):
     car.append(value**2)
 print(car)
+
+# list中使用循环赋值
+moto = [value**2 for value in range(1,15)]
+print(moto)
 
