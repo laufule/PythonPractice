48d4f9eee0049efc06d4d5847a25fa62083d7f1c
laufule <laufule@gmail.com>
2020-05-14 23:12:29 +0800

    list-loops

---
+++ 4.2-list.loop.py
@@ -0,0 +1,10 @@
+# loops for python
+bickles = ['one','two','three','four','five']
+for bickle in bickles:
+    print(bickle)
+
+car = []
+for value in range(1,10):
+    car.append(value**2)
+print(car)
+
